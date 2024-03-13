5636e356460911a21ab05351514fb4e61e38e2b6
Wojciech Gajda <wojciech.gajda@mirumee.com>
2024-03-13 07:20:26 +0100

    products list

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [{ hostname: "storefront-nextjs-wine.vercel.app" }],
+	},
+};
 
 export default nextConfig;
