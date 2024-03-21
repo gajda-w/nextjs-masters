45d19053061c43417e249dbba8886d5a805ca9ba
Wojciech Gajda <wojciech.gajda@mirumee.com>
2024-03-21 16:25:50 +0100

    add stripe payments

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import Stripe from "stripe";
 import { CartList } from "@/components/organisms/CartList";
 import { getCartById } from "@/api/cart";
 
@@ -16,12 +17,65 @@ export default async function CartPage() {
 		redirect("/");
 	}
 
+	async function handleStripePaymentAction() {
+		"use server";
+
+		if (!process.env.STRIPE_SECRET_KEY) {
+			throw new Error("Missing STRIPE_SECRET_KEY env variable");
+		}
+
+		const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+			apiVersion: "2023-10-16",
+			typescript: true,
+		});
+
+		const cart = await getCartById(cartId || "");
+
+		if (!cart) {
+			return;
+		}
+
+		const session = await stripe.checkout.sessions.create({
+			metadata: {
+				cartId: cart.id,
+			},
+			line_items: cart.items.map((item) => ({
+				price_data: {
+					currency: "usd",
+					product_data: {
+						name: item.product.name,
+						// TODO add product description
+						description: item.product.name,
+						images: item.product.images.map((i) => i.url),
+					},
+					unit_amount: item.product.price,
+				},
+				quantity: item.quantity,
+			})),
+			mode: "payment",
+			success_url: `http://localhost:3000/cart/success?session_id={CHECKOUT_SESSION_ID}`,
+			cancel_url: `http://localhost:3000/cart/canceled`,
+		});
+		if (session.url) {
+			cookies().set("cartId", "");
+			redirect(session.url);
+		}
+	}
+
 	return (
 		<>
 			<h1>Order #{cart.id} summary</h1>
 			<div className="mt-8 flow-root max-w-lg">
 				<CartList items={cart.items} />
 			</div>
+			<form action={handleStripePaymentAction} className="ml-auto">
+				<button
+					type="submit"
+					className="rounded-sm border  px-8 py-2 shadow-sm transition-colors hover:bg-slate-200"
+				>
+					Pay
+				</button>
+			</form>
 		</>
 	);
 }
